571183c1da1d31891f9258e05a1d22b33c08e28a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-01 21:47:38 +0300

    feat - display product on home page

--- shopify-nextjs/pages/index.js
+++ shopify-nextjs/pages/index.js
@@ -1,11 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
-import { getProductsInCollection } from "../lib/shopify";
 import styles from "../styles/Home.module.css";
+import { getProductsInCollection } from "../lib/shopify";
+import ProductList from "../components/ProductList";
 
 export default function Home({ products }) {
   console.log(products);
-  return <div className="text-3xl">products</div>;
+  return (
+    <div className="text-3xl">
+      <ProductList products={products} />
+    </div>
+  );
 }
 
 export async function getStaticProps() {
