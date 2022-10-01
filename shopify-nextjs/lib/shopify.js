bdfff05b810e9f431278efe0c53b5f6243b8b8a0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-01 20:56:44 +0300

    backend - add get shopify data function

---
+++ shopify-nextjs/lib/shopify.js
@@ -0,0 +1,27 @@
+const domain = process.env.SHOPIFY_STORE_DOMAIN;
+const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN;
+
+async function ShopifyData(query) {
+  const URL = `https://${domain}/api/2022-10/graphql.json`;
+
+  const options = {
+    endpoint: URL,
+    method: "POST",
+    headers: {
+      "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  };
+
+  try {
+    const data = await fetch(URL, options).then((response) => {
+      return response.json();
+    });
+
+    return data;
+  } catch (error) {
+    throw new Error("Products not fetched");
+  }
+}
