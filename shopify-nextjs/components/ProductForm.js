9a3fc146a57ae87265c06f0969d5e8d33e0f4ed8
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-02 10:31:31 +0300

    feat - add product form component

---
+++ shopify-nextjs/components/ProductForm.js
@@ -0,0 +1,58 @@
+import { useState, useEffect, useContext } from "react";
+import { formatter } from "../utils/helpers";
+import ProductOptions from "./ProductOptions";
+
+export default function ProductForm({ product }) {
+  //   console.log(product);
+  const allVariantOptions = product.variants.edges?.map((variant) => {
+    const allOptions = {};
+
+    // create object of selected items
+    variant.node.selectedOptions.map((item) => {
+      allOptions[item.name] = item.value;
+    });
+
+    return {
+      id: variant.node.id,
+      title: product.title,
+      handle: product.handle,
+      image: variant.node.image?.url,
+      options: allOptions,
+      variantTitle: variant.node.title,
+      variantPrice: variant.node.priceV2.amount,
+      variantQuantity: 1,
+    };
+  });
+
+  const defaultValues = {};
+  product.options.map((item) => {
+    defaultValues[item.name] = item.values[0];
+  });
+
+  const [selectedVariant, setSelectedVariant] = useState(allVariantOptions[0]);
+  const [selectedOptions, setSelectedOptions] = useState(defaultValues);
+
+  function setOptions(name, value) {
+    setSelectedOptions((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  }
+
+  return (
+    <div className="flex flex-col rounded-2xl p-4 shadow-lg md:w-1/3">
+      <h2 className="text-2xl font-bold">{product.title}</h2>
+      <span className="pb-6">
+        {formatter.format(product.variants.edges[0].node.priceV2.amount)}
+      </span>
+      {product.options.map(({ name, values }) => (
+        <ProductOptions
+          key={`key-${name}`}
+          name={name}
+          values={values}
+          selectedOptions={selectedOptions}
+          setOptions={setOptions}
+        />
+      ))}
+    </div>
+  );
+}
