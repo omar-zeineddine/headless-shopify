a0e2481dbe4917b2df63f4c3b053803baad4f7d0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-02 20:39:44 +0300

    feat - add hero section

--- shopify-nextjs/pages/index.js
+++ shopify-nextjs/pages/index.js
@@ -3,10 +3,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { getProductsInCollection } from "../lib/shopify";
 import ProductList from "../components/ProductList";
+import Hero from "../components/Hero";
 
 export default function Home({ products }) {
   return (
     <div className="text-3xl">
+      <Hero />
       <ProductList products={products} />
     </div>
   );
